fbcbf6d2d82e4a29b9328cb0c5be220de697d514
Abhilash31304 <115718901+Abhilash31304@users.noreply.github.com>
2024-12-09 10:46:24 +0530

    emotionalLabel.py

--- emotionalLabel.py
+++ emotionalLabel.py
@@ -4,13 +4,7 @@ import torch
 
 device = 0 if torch.cuda.is_available() else -1
 
-emotion_classifier = pipeline(
-    "text-classification",
-    model="bhadresh-savani/bert-base-uncased-emotion",
-    max_length=512,
-    truncation=True,
-    device=device
-)
+emotion_classifier = pipeline("text-classification",model="bhadresh-savani/bert-base-uncased-emotion",max_length=512,truncation=True,device=device)
 
 pdf = pd.read_csv('/kaggle/working/processed_lyrics.csv')
 sample_pdf = pdf.sample(n=20000, random_state=42).reset_index(drop=True)
